193232d3d6a1f46088c92d943a8aa97d5b498d8f
Forest Gregg <fgregg@uchicago.edu>
2015-04-15 15:57:40 -0500

    starting on lda

---
+++ code/lda_sic.R
@@ -0,0 +1,16 @@
+library(slam)
+library(topicmodels)
+
+businesses <- read.csv("../data/geocoded_businesses.csv")
+sic <- read.csv("../data/sic.csv")
+
+businesses$zip5 <- factor(substr(businesses$ZIP, 1, 5))
+businesses$sic <- sic[na.omit(match(businesses$SIC, sic$code)), "name"]
+
+zip_sic_M <- slam::as.simple_triplet_matrix(table(businesses$zip5,
+                                                  businesses$sic))
+
+model_1 <- topicmodels::LDA(zip_sic_M, 10)
+
+topics <- terms(model_1, 10)
+
